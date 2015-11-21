169a52a4d963fdce1b1d147067c774579aa5d28e
Thibaud Dauce <thibaud@dauce.fr>
2015-11-21 18:48:11 +0100

    clean up code

--- app/Main.hs
+++ app/Main.hs
@@ -3,29 +3,8 @@
 module Main where
 
 import Lib
-import qualified Data.ByteString.Char8 as B
-import Data.Maybe (isJust)
-import Crypto.Hash
-import Control.Parallel.Strategies
-
-sha1 :: B.ByteString -> Digest SHA1
-sha1 = hash
-
-allStrings :: [String]
-allStrings = [ c : s | s <- "" : allStrings, c <- ['a'..'z'] ++ ['0'..'9'] ]
-
-checkPassword hash string
-  | (sha1 $ B.pack string) == hash = Just string
-  | otherwise = Nothing
-
-getPassword :: Maybe String -> Maybe String -> Maybe String
-getPassword Nothing maybePassword = maybePassword
-getPassword maybePassword Nothing = maybePassword
 
 main :: IO ()
 main = do
-  let passwordHash = sha1 $ B.pack "zaim9"
-  -- runEval $ parBuffer 10000 rseq $
-  let maybePasswords = map (checkPassword passwordHash) allStrings
-  let findPassword = head $ filter isJust maybePasswords
-  putStrLn $ show findPassword
+  let passwordHash = sha1 "zaim9"
+  putStrLn $ findPassword passwordHash
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,28 @@
 module Lib
-    ( someFunc
+    ( findPassword
+    , sha1
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Data.ByteString.Char8 as B
+import Data.Maybe (isJust)
+import Crypto.Hash
+import Control.Parallel.Strategies
+
+sha1 :: String -> Digest SHA1
+sha1 = hash . B.pack
+
+allStrings :: [String]
+allStrings = [ c : s | s <- "" : allStrings, c <- ['a'..'z'] ++ ['0'..'9'] ]
+
+checkPassword :: Digest SHA1 -> String -> Maybe String
+checkPassword hash string
+  | (sha1 string) == hash = Just string
+  | otherwise = Nothing
+
+getPassword :: Maybe String -> Maybe String -> Maybe String
+getPassword Nothing maybePassword = maybePassword
+getPassword maybePassword Nothing = maybePassword
+
+-- runEval $ parBuffer 10000 rseq $
+findPassword :: Digest SHA1 -> String
+findPassword passwordHash = show $ head $ filter isJust $ map (checkPassword passwordHash) allStrings
