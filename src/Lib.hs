d8bf4ec3b162b15d424cf7305f4a45d7c0225cdb
Thibaud Dauce <thibaud@dauce.fr>
2015-11-21 20:29:18 +0100

    clean up code

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,7 +7,6 @@ import qualified Data.ByteString.Char8 as B
 import Data.Maybe (isJust)
 import Crypto.Hash
 import qualified Control.Parallel.Strategies as S
-import Control.Parallel.Strategies (NFData, ($||))
 
 sha1 :: String -> Digest SHA1
 sha1 = hash . B.pack
@@ -15,12 +14,11 @@ sha1 = hash . B.pack
 allStrings :: [String]
 allStrings = [ c : s | s <- "" : allStrings, c <- ['a'..'z'] ++ ['0'..'9'] ]
 
-parFilter :: (NFData a) => (a -> Bool) -> [a] -> [a]
-parFilter p = S.withStrategy (S.parBuffer 100 S.rseq) . filter p
+parFilter :: (S.NFData a) => (a -> Bool) -> [a] -> [a]
+parFilter p = S.withStrategy (S.evalBuffer 1000 S.rseq) . filter p
 
 checkPassword :: Digest SHA1 -> String -> Bool
 checkPassword hash string = (sha1 string) == hash
 
 findPassword :: Digest SHA1 -> String
-findPassword passwordHash = (head . filter checkThePassword) allStrings
-  where checkThePassword = checkPassword passwordHash
+findPassword passwordHash = (head . filter (checkPassword passwordHash)) allStrings
